35b5fcba1e4486403e012a0b3fb14818285c29cf
Libby Marchetti <lsmarchetti01@gmail.com>
2022-04-13 17:49:40 -0500

    started file structure

--- index.js
+++ index.js
@@ -1,62 +1,64 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer')
 const fs = require('fs')
+const generateMarkdown = require('./utils/generateMarkdown.js')
 // TODO: Create an array of questions for user input
 const questions = [
     
         {
             type: 'input',
             name: 'title',
-            message: 'What is the title of your project?'
+            message: 'What is the title of the project?',
+            
         },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Give a brief description of your project',        
-        },
-        {
-            type: 'confirm',
-            name: 'toc',
-            message: 'Would you like a table of contents?'
-        },
-        {
-            type: 'input',
-            name: 'installation',
-            message: 'What are the instatllation instructions for your project?'
-        },
-        {
-            type: 'input',
-            name: 'usage',
-            message: 'How is your project meant to be used?'
-        },
-        {
-            type: 'confirm',
-            name: 'license',
-            message: 'Would you like to add an MIT license?'
-        },
-        {
-            type: 'input',
-            name: 'contributors',
-            message: 'List constributor if any.'
-        },
-        {
-            type: 'input',
-            name: 'tests',
-            message: 'Come back and figure this one out!!'
-        },
-        {
-            type: 'input',
-            name: 'questions',
-            message: 'Come back to this one too!!'
-        },   
+        // {
+        //     type: 'input',
+        //     name: 'description',
+        //     message: 'Give a brief description of the project',        
+        // },
+        // {
+        //     type: 'confirm',
+        //     name: 'toc',
+        //     message: 'Would you like a table of contents?'
+        // },
+        // {
+        //     type: 'input',
+        //     name: 'installation',
+        //     message: 'What are the instatllation instructions for the project?'
+        // },
+        // {
+        //     type: 'input',
+        //     name: 'usage',
+        //     message: 'How is the project meant to be used?'
+        // },
+        // {
+        //     type: 'list',
+        //     name: 'license',
+        //     message: 'Would you like to add a license?',
+        //     choices: ['MIT', 'Someother'],
+        //     default: 'MIT'
+        // },
+        // {
+        //     type: 'input',
+        //     name: 'contributors',
+        //     message: 'List anyone who contributed on the project.'
+        // },
+        // {
+        //     type: 'input',
+        //     name: 'tests',
+        //     message: 'Come back and figure this one out!!'
+        // },
+        // {
+        //     type: 'input',
+        //     name: 'questions',
+        //     message: 'Come back to this one too!!'
+        // },   
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
 
-    const generateREADME = (data) =>
-
-    ``
+    
     
 }
 
